71f5cc95ee71b06aac8844b98be838f207b5eec7
TanyaArdhia <tanyaardhiap@gmail.com>
2024-07-17 22:45:08 +0700

    fix: card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,34 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/Card.css";
 
 export function Card({ product }) {
-  return (
-    <div className="card">
-      <img src={product.image} alt={product.title} className="card-image" />
-      <div className="card-content">
-        <h3 className="card-title">{product.title}</h3>
-        <div className="price">
-          <p className="card-price">${product.price.toFixed(2)}</p>
-          <button className="cart-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="icon size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
-          </button>
-        </div>
+  const [isInCart, setIsInCart] = useState(false)
+  const [isInWishlist, setIsInWishlist] = useState(false);
+  
 
-        <div>
-          <button className="wishlist-button">
+  const truncatedDescription = product.description.length > 100 
+  ? product.description.substring(0, 100) + "..." 
+  : product.description;
+
+  const addToCart = () => {
+    setIsInCart(true);
+    setTimeout(() => setIsInCart(false), 1000);
+
+    console.log(`${product.title} added to cart`);
+  };
+  
+  const addToWishlist = () => {
+    setIsInWishlist(true);
+    setTimeout(() => setIsInWishlist(false), 1000);
+
+    console.log(`${product.title} ${isInWishlist ? "removed from" : "added to"} wishlist`);
+  };
+
+  return (
+    <div className="card-container">
+      <div className="card">
+        <img src={product.image} alt={product.title} className="card-image" />
+        <div className="card-content">
+          <h3 className="card-title">{product.title}</h3>
+          <p className="card-description">{truncatedDescription}</p>
+          <div className="price">
+            <p className="card-price">${product.price.toFixed(2)}</p>
+            <button className="cart-button" onClick={addToCart} disabled={isInCart}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="icon size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+            </button>
+          </div>
+          <button className={`wishlist-button ${isInWishlist ? 'active' : ''}`} onClick={addToWishlist}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
